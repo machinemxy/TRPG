7c3e62d86523643a4ba31c1a4e544d1d12df8c67
Ma Xueyuan <machinemxy@me.com>
2020-08-09 02:04:16 +0900

    Enable to show the first map

---
+++ TRPG/MapViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MapViewController.swift
+//  TRPG
+//
+//  Created by Ma Xueyuan on 2020/08/09.
+//  Copyright © 2020 Ma Xueyuan. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "Village") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscape
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return false
+    }
+}
+
---
+++ TRPG/Scenes/Village.swift
@@ -0,0 +1,14 @@
+//
+//  Village.swift
+//  TRPG
+//
+//  Created by Ma Xueyuan on 2020/08/09.
+//  Copyright © 2020 Ma Xueyuan. All rights reserved.
+//
+
+import SpriteKit
+
+class Village: SKScene {
+    override func didMove(to view: SKView) {
+    }
+}
